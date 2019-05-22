a59ba167fca1ec9e190962e14e286a888be31907
luangpoomyut <kevinluangpoomyut@gmail.com>
2019-05-22 14:40:10 -0500

    Able to reset game with new colors

--- Color Guessing Game/colorGame.js
+++ Color Guessing Game/colorGame.js
@@ -5,6 +5,7 @@ var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
 
 colorDisplay.textContent = pickedColor;
 
@@ -19,6 +20,7 @@ for (var i = 0; i < squares.length; i++) {
         // compare clicked to picked color
         if (clickedColor === pickedColor) {
             messageDisplay.textContent = "Correct!";
+            resetButton.textContent = "Play Again?";
             changeColors(clickedColor);
             h1.style.backgroundColor = clickedColor;
         }
@@ -28,6 +30,18 @@ for (var i = 0; i < squares.length; i++) {
         }
     });
 }
+
+resetButton.addEventListener("click", function() {
+    colors = generateRandomColors(6);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = colors[i];
+    }
+
+    h1.style.background = "#232323";
+});
 
 function changeColors(color) {
     // iterate through all squares
